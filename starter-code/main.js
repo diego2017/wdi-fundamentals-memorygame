2107d234808da0f4cbc1108b81d866270b1b32c1
Diego <diego_v_f@hotmail.com>
2017-02-12 14:37:22 +1100

    unit 10 update

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,26 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-if (cardOne === cardTwo) {
-	alert('You found a match')
-} else if (cardThree === cardFour) {
-	alert ('You found a match')
-} else if (cardOne !== cardThree) {
-	alert ('Sorry, try again')
-}
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
+// if (cardOne === cardTwo) {
+//	alert('You found a match')
+// } else if (cardThree === cardFour) {
+// 	alert ('You found a match')
+// } else if (cardOne !== cardThree) {
+//	alert ('Sorry, try again')
+//} 
+
+var gameBoard = document.getElementById('game-board');
+var createCards = function () {
+
+	var cards = 4;
+for (var i=0; i<cards.length; i++) {
+    var cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    gameBoard.appendChild(cardDiv);
+  }
+ }
+
+	
